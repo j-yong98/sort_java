b0b8e5db3438720ad33548a1d3f7738b1aa12740
j-yong98 <chlwodyd9829@gmail.com>
2022-12-14 20:07:37 +0900

    feat: 선택 정렬 구현

--- src/main/java/SelectionSort.java
+++ src/main/java/SelectionSort.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class SelectionSort {
+public class SelectionSort {
+
+    public int[] selection(int n, int[] list) {
+        for (int i = 0; i < n; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (list[min] > list[j]) {
+                    min = j;
+                }
+            }
+            int tmp = list[i];
+            list[i] = list[min];
+            list[min] = tmp;
+        }
+        return list;
+    }
 }
