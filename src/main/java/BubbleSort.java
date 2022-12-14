6b7388868d90fd6fa1fd2b2277e6380a6efb5f2b
j-yong98 <chlwodyd9829@gmail.com>
2022-12-14 20:06:57 +0900

    list -> array

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -1,14 +1,11 @@
-import java.util.List;
-import java.util.Scanner;
-
 public class BubbleSort {
-    public List<Integer> bubble(int n, List<Integer> list) {
+    public int[] bubble(int n, int[] list) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n - 1; j++) {
-                if (list.get(j) > list.get(j + 1)) {
-                    int tmp = list.get(j);
-                    list.set(j, list.get(j + 1));
-                    list.set(j + 1, tmp);
+                if (list[j] > list[j + 1]) {
+                    int tmp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = tmp;
                 }
             }
         }
