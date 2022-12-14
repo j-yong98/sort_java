c0ee47f64a2df1108dea53dd9ca72123c89730e6
j-yong98 <chlwodyd9829@gmail.com>
2022-12-14 21:19:25 +0900

    test: 기수 정렬 테스트

--- src/test/java/sortTest.java
+++ src/test/java/sortTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class sortTest {
@@ -33,5 +34,17 @@ public class sortTest {
         int[] insertion = insertionSort.insertion(6, new int[]{10, 5, 6, 3, 8, 2});
 
         assertThat(insertion).isEqualTo(new int[]{2,3,5,6,8,10});
+    }
+
+    @Test
+    @DisplayName("기수 정렬 테스트")
+    void radixSort(){
+        RadixSort radixSort = new RadixSort();
+
+        int[] radix1 = radixSort.radix(6, new int[]{5, 8, 4, 6, 2, 1}, 1);
+        int[] radix2 = radixSort.radix(6,new int[]{43,33,28,61,15,97},2);
+
+        assertThat(radix1).isEqualTo(new int[]{1,2,4,5,6,8});
+        assertThat(radix2).isEqualTo(new int[]{15,28,33,43,61,97});
     }
 }
